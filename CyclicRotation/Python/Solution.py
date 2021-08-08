de2ce3b47667546915c4b2ba51d0ab3ca0e67f41
Amir Hatami <am.hatami@gmail.com>
2021-08-08 12:31:42 -0700

    Update Solution.py

--- CyclicRotation/Python/Solution.py
+++ CyclicRotation/Python/Solution.py
@@ -1,1 +1,10 @@
-// todo  
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+
+def solution(A, K):     
+   if K == 0 or len(A)/K ==0:
+      return A
+   if K > len(A):
+      K = K/len(A)
+   A = A[len(A)-K:len(A)] + (A[0:len(A)-K])
+   return A
